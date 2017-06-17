b2207b4a25c403cc222e8a5c6a9a395d4bd49ffc
lerycibalio <lerycibalio@gmail.com>
2017-06-18 01:40:21 +0800

    updated sql.

--- sql/msadb.sql
+++ sql/msadb.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: msadb
 -- ------------------------------------------------------
 -- Server version	5.7.13-log
-
+-- n
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
