a2f6b783aaa2d38c156b9bd0a00d6f80e127f970
lerycibalio <lerycibalio@gmail.com>
2017-06-23 14:21:35 +0800

    sql

---
+++ sql/scripts/view_guardsbycliant.sql
@@ -0,0 +1,14 @@
+select 
+                        guards.gid, dutydetails.did, sduty_assignment.aid,
+                        concat(ln,', ',fn,' ',mn) as name,
+                        concat(streetno, ', ', streetname, ', ', brgy, ', ', city) as Location,
+                        case 
+	                        when concat(timein,'-', timeout,' ', days) is null then 'Unscheduled'
+                            else concat(timein,'-', timeout,' ', days)
+                            end as schedule
+                         from guards 
+                        left join sduty_assignment on sduty_assignment.gid=guards.gid
+                        left join dutydetails on sduty_assignment.aid=dutydetails.aid
+                        left join request_assign on request_assign.raid=sduty_assignment.raid
+                        left join request on request_assign.rid=request.rid
+                        group by gid;
