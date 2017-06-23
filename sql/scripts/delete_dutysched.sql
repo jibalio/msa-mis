1b37714b9a45ad00245b4c444c83d5b9bcd891ff
lerycibalio <lerycibalio@gmail.com>
2017-06-24 00:04:17 +0800

    sql

--- sql/scripts/delete_dutysched.sql
+++
@@ -1,8 +0,0 @@
-
-delete from request_assign;
-delete from request_dismiss;
-delete from request;
-delete from dutydetails;
-delete from sduty_assignment;
-delete from incidentreport;
-
--- sql/scripts/view_guardstobedismissed.sql
+++
@@ -1,4 +0,0 @@
-select concat(ln,', ',fn,' ',mn) as name from guards 
-                                            left join sduty_assignment on sduty_assignment.GID = guards.gid
-                                            left join request_dismiss on request_dismiss.gid = guards.gid
-                                            where rid = 2;
