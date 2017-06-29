04cf49d3b98de026e007ccd0936279f2fad0825a
lerycibalio <lerycibalio@gmail.com>
2017-06-29 19:27:43 +0800

    Included db

--- sql/msadb.sql
+++ sql/msadb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `msadb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `msadb`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
 --
 -- Host: 127.0.0.1    Database: msadb
@@ -605,4 +607,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-29 19:06:38
+-- Dump completed on 2017-06-29 19:27:09
