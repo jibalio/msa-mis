0020a07f0387b921cddce405db24ade86796ee55
lerycibalio <lerycibalio@gmail.com>
2017-06-30 15:43:55 +0800

    re-Added rydb.sqltools

---
+++ src/msamis/ryldb.sqltools/Client.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSAMISUserInterface {
+    public class Client {
+
+        public static DataTable GetClients() {
+            DataTable dt = new DataTable();
+            String query = "select cid, name from client;";
+            return SQLTools.ExecuteQuery(query);
+        }
+    }
+}
