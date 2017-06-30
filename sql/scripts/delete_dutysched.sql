877af565f524c7fa770f9144da0d83b10d6c5352
lerycibalio <lerycibalio@gmail.com>
2017-06-30 16:14:25 +0800

    added 2 new sql scripts

--- sql/scripts/delete_dutysched.sql
+++ sql/scripts/delete_dutysched.sql
@@ -6,3 +6,9 @@ delete from dutydetails;
 delete from sduty_assignment;
 delete from incidentreport;
 
+alter table request_assign auto_increment=1;
+alter table request_unassign auto_increment=1;
+alter table request auto_increment=1;
+alter table request_assign auto_increment=1;
+alter table sduty_assignment auto_increment=1;
+alter table incidentreport auto_increment=1;
