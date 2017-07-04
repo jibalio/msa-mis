80ba6893a496e7ec82cc0befd08b150dc3d4b9c2
lerycibalio <lerycibalio@gmail.com>
2017-07-04 13:22:41 +0800

    Fix null timein error

--- src/msamis/ryldb.sqltools/Attendance.cs
+++ src/msamis/ryldb.sqltools/Attendance.cs
@@ -187,8 +187,8 @@ namespace MSAMISUserInterface {
             String q = @"
                             select atid, dutydetails.did, DATE_FORMAT(date, '%Y-%m-%d') as Date, SUBSTRING(DAYNAME(DATE_FORMAT(date, '%Y-%m-%d')) FROM 1 FOR 3)  as day, 
 							concat (ti_hh,':',ti_mm,' ',ti_period, ' - ',to_hh,':',to_mm,' ',to_period) as Schedule,
-                            case when timein is null then 'Not set' when timein is not null then timein end as TimeIn,
-                            case when timeout is null then 'Not set' when timeout is not null then timeout end as TimeOut, hours, 
+                            timein,
+                           TimeOut, hours, 
                             night as NightHours, overtime,
                             case holiday when 1 then 'Yes' when 0 then 'No' end as Holiday
                             from attendance
