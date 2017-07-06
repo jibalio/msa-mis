2c343e82de92b870f2381e2ac17d00e627dac90d
AJ Pasigado <ajpasigado@outlook.com>
2017-07-06 10:30:39 +0800

    [Misc] View DisReq Redesign

--- src/msamis/MSAMISUserInterface/Sched_ViewDisReq.Designer.cs
+++ src/msamis/MSAMISUserInterface/Sched_ViewDisReq.Designer.cs
@@ -27,6 +27,7 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sched_ViewDisReq));
             this.FadeTMR = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
             this.NameLBL = new System.Windows.Forms.Label();
@@ -35,8 +36,10 @@
             this.AssignedGRD = new System.Windows.Forms.DataGridView();
             this.CloseBTN = new System.Windows.Forms.Button();
             this.ApproveBTN = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.AssignedGRD)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // FadeTMR
@@ -46,24 +49,23 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.NameLBL);
             this.panel1.Controls.Add(this.ClientLBL);
             this.panel1.Controls.Add(this.label2);
             this.panel1.ForeColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(2, 56);
+            this.panel1.Location = new System.Drawing.Point(2, 43);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(718, 137);
+            this.panel1.Size = new System.Drawing.Size(545, 71);
             this.panel1.TabIndex = 125;
             // 
             // NameLBL
             // 
-            this.NameLBL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.NameLBL.AutoSize = true;
             this.NameLBL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.NameLBL.ForeColor = System.Drawing.Color.White;
-            this.NameLBL.Location = new System.Drawing.Point(0, 109);
+            this.NameLBL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
+            this.NameLBL.Location = new System.Drawing.Point(174, 20);
             this.NameLBL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.NameLBL.Name = "NameLBL";
-            this.NameLBL.Size = new System.Drawing.Size(718, 21);
+            this.NameLBL.Size = new System.Drawing.Size(201, 21);
             this.NameLBL.TabIndex = 145;
             this.NameLBL.Text = "Guards to be unassigned:";
             this.NameLBL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -76,7 +78,7 @@
             this.ClientLBL.Location = new System.Drawing.Point(0, 26);
             this.ClientLBL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ClientLBL.Name = "ClientLBL";
-            this.ClientLBL.Size = new System.Drawing.Size(718, 83);
+            this.ClientLBL.Size = new System.Drawing.Size(545, 83);
             this.ClientLBL.TabIndex = 120;
             this.ClientLBL.Text = "Laboriki Enterprises";
             this.ClientLBL.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -89,7 +91,7 @@
             this.label2.Location = new System.Drawing.Point(0, 0);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(718, 26);
+            this.label2.Size = new System.Drawing.Size(545, 26);
             this.label2.TabIndex = 121;
             this.label2.Text = "REQUEST BY";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -100,7 +102,7 @@
             this.AssignedGRD.AllowUserToDeleteRows = false;
             this.AssignedGRD.AllowUserToResizeColumns = false;
             this.AssignedGRD.AllowUserToResizeRows = false;
-            this.AssignedGRD.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
+            this.AssignedGRD.BackgroundColor = System.Drawing.Color.White;
             this.AssignedGRD.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.AssignedGRD.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.AssignedGRD.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
@@ -115,15 +117,15 @@
             this.AssignedGRD.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.AssignedGRD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Semilight", 10F);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(109)))), ((int)(((byte)(140)))));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.AssignedGRD.DefaultCellStyle = dataGridViewCellStyle2;
             this.AssignedGRD.EnableHeadersVisualStyles = false;
-            this.AssignedGRD.Location = new System.Drawing.Point(158, 199);
+            this.AssignedGRD.Location = new System.Drawing.Point(74, 58);
             this.AssignedGRD.MultiSelect = false;
             this.AssignedGRD.Name = "AssignedGRD";
             this.AssignedGRD.ReadOnly = true;
@@ -137,14 +139,13 @@
             this.AssignedGRD.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.AssignedGRD.RowHeadersVisible = false;
             this.AssignedGRD.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.AssignedGRD.Size = new System.Drawing.Size(400, 357);
+            this.AssignedGRD.Size = new System.Drawing.Size(400, 305);
             this.AssignedGRD.TabIndex = 144;
             // 
             // CloseBTN
             // 
             this.CloseBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.CloseBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
-            this.CloseBTN.BackgroundImage = global::MSAMISUserInterface.Properties.Resources.BTNV2;
+            this.CloseBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
             this.CloseBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CloseBTN.FlatAppearance.BorderSize = 0;
             this.CloseBTN.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
@@ -152,19 +153,19 @@
             this.CloseBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CloseBTN.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CloseBTN.ForeColor = System.Drawing.Color.White;
-            this.CloseBTN.Location = new System.Drawing.Point(360, 600);
+            this.CloseBTN.Location = new System.Drawing.Point(513, 1);
             this.CloseBTN.Name = "CloseBTN";
-            this.CloseBTN.Size = new System.Drawing.Size(105, 32);
+            this.CloseBTN.Size = new System.Drawing.Size(34, 32);
             this.CloseBTN.TabIndex = 147;
-            this.CloseBTN.Text = "CLOSE";
+            this.CloseBTN.Text = "X";
             this.CloseBTN.UseVisualStyleBackColor = false;
             this.CloseBTN.Click += new System.EventHandler(this.CloseBTN_Click);
             // 
             // ApproveBTN
             // 
             this.ApproveBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.ApproveBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(109)))), ((int)(((byte)(140)))));
-            this.ApproveBTN.BackgroundImage = global::MSAMISUserInterface.Properties.Resources.BTNV2;
+            this.ApproveBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
+            this.ApproveBTN.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ApproveBTN.BackgroundImage")));
             this.ApproveBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ApproveBTN.FlatAppearance.BorderSize = 0;
             this.ApproveBTN.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(87)))), ((int)(((byte)(112)))));
@@ -172,7 +173,7 @@
             this.ApproveBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ApproveBTN.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ApproveBTN.ForeColor = System.Drawing.Color.White;
-            this.ApproveBTN.Location = new System.Drawing.Point(249, 600);
+            this.ApproveBTN.Location = new System.Drawing.Point(234, 382);
             this.ApproveBTN.Name = "ApproveBTN";
             this.ApproveBTN.Size = new System.Drawing.Size(105, 32);
             this.ApproveBTN.TabIndex = 146;
@@ -180,17 +181,27 @@
             this.ApproveBTN.UseVisualStyleBackColor = false;
             this.ApproveBTN.Click += new System.EventHandler(this.ApproveBTN_Click);
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.ApproveBTN);
+            this.panel2.Controls.Add(this.NameLBL);
+            this.panel2.Controls.Add(this.AssignedGRD);
+            this.panel2.Location = new System.Drawing.Point(0, 155);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(557, 445);
+            this.panel2.TabIndex = 148;
+            // 
             // Sched_ViewDisReq
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(64)))), ((int)(((byte)(82)))));
-            this.ClientSize = new System.Drawing.Size(721, 666);
+            this.ClientSize = new System.Drawing.Size(548, 598);
             this.ControlBox = false;
             this.Controls.Add(this.CloseBTN);
-            this.Controls.Add(this.ApproveBTN);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.AssignedGRD);
+            this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Sched_ViewDisReq";
             this.ShowInTaskbar = false;
@@ -199,6 +210,8 @@
             this.Load += new System.EventHandler(this.Sched_ViewDisReq_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.AssignedGRD)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -213,5 +226,6 @@
         private System.Windows.Forms.DataGridView AssignedGRD;
         private System.Windows.Forms.Button CloseBTN;
         private System.Windows.Forms.Button ApproveBTN;
+        private System.Windows.Forms.Panel panel2;
     }
 }
