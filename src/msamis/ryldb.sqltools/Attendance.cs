ea5ed666634e82ca0021fa955ee0362d155e63d6
lerycibalio <lerycibalio@gmail.com>
2017-07-07 15:54:43 +0800

    [Fixed] Original GetAttendance restored

--- src/msamis/ryldb.sqltools/Attendance.cs
+++ src/msamis/ryldb.sqltools/Attendance.cs
@@ -188,10 +188,10 @@ namespace MSAMISUserInterface {
 
         public DataTable GetAttendance() {
             String q = @"
-                        select atid, dutydetails.did, 
-							CONCAT((DATE_FORMAT(date, '%d')), ' / ' ,
-							(CONCAT (ti_hh,':',ti_mm,' ',SUBSTRING(ti_period,1,1), '-',to_hh,':',to_mm,SUBSTRING(to_period,1,1)))) as Schedule,
-                            concat( SUBSTRING(timein,1,7), '-' ,SUBSTRING(timeout,1,7)) as timein,  hours, 
+                        select atid, dutydetails.did, DATE_FORMAT(date, '%Y-%m-%d') as Date, SUBSTRING(DAYNAME(DATE_FORMAT(date, '%Y-%m-%d')) FROM 1 FOR 3)  as day, 
+							concat (ti_hh,':',ti_mm,' ',ti_period, ' - ',to_hh,':',to_mm,' ',to_period) as Schedule,
+                            timein,
+                           TimeOut, hours, 
                             night as NightHours, overtime,
                             case holiday when 1 then 'Yes' when 0 then 'No' end as Holiday
                             from attendance
