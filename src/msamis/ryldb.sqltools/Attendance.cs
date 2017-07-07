7c5034d70b83760c0b83061c2483993eda11126a
lerycibalio <lerycibalio@gmail.com>
2017-07-07 16:38:07 +0800

    attendance

--- src/msamis/ryldb.sqltools/Attendance.cs
+++ src/msamis/ryldb.sqltools/Attendance.cs
@@ -254,7 +254,6 @@ namespace MSAMISUserInterface {
         public static double GetNightHours(DateTime actuals, DateTime actuale) {
             DateTime NightStart = new DateTime(1, 1, 1, 22, 00, 00);
             DateTime NightEnd = new DateTime(1, 2, 1, 6, 00, 00);
-            DateTime Midnight = new DateTime(1, 2, 1, 00, 00, 00);
             if (actuals > actuale) {
                 actuale = actuale.AddDays(1);
                 NightEnd = NightEnd.AddDays(1);
@@ -264,7 +263,12 @@ namespace MSAMISUserInterface {
                 double returnValue = interval > TimeSpan.FromSeconds(0) ? interval.TotalMinutes : 0;
                 return returnValue;
             } else {
+
+                NightEnd = new DateTime(1, 1, 1, 6, 00, 00);
                 DateTime maxStart = actuals < NightStart ? actuals : NightStart;
+
+              
+
                 DateTime minEnd = actuale < NightEnd ? actuale : NightEnd;
                 TimeSpan interval = minEnd - maxStart;
                 double returnValue = interval > TimeSpan.FromSeconds(0) ? interval.TotalMinutes : 0;
