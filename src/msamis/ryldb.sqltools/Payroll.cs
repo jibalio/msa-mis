8d5e927a9ea7f05c4d03b69973d89d954243aeb8
lerycibalio <lerycibalio@gmail.com>
2017-07-13 10:23:59 +0800

    [Added] Payroll.cs: AddBasicPay()

--- src/msamis/ryldb.sqltools/Payroll.cs
+++ src/msamis/ryldb.sqltools/Payroll.cs
@@ -10,7 +10,18 @@ namespace MSAMISUserInterface {
 
         public static double BasicPay = 340.00;
 
-        
+        public static void AddBasicPay(DateTime start, double pay) {
+            String q;
+            q = @"INSERT INTO `msadb`.`basicpay` (`amount`, `start`, `end`, `status`) VALUES ('{0}', '{1}', '{2}', '{3}');";
+            string status;
+            DateTime sta = new DateTime(start.Year, start.Month, start.Day);
+            DateTime end = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            // Case 1: If karon gamiton
+            if (sta < end) status = "0";
+            else status = "1";
+            q = String.Format(q, sta.ToString("yyyy-MM-dd"), "", status);
+            SQLTools.ExecuteNonQuery(q);
+        }
 
     }
 
