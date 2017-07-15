247345deebcc7221a1bfd9da7237444be720b95b
lerycibalio <lerycibalio@gmail.com>
2017-07-15 10:46:01 +0800

    Added GetGuardsPayrollList()

--- src/msamis/ryldb.sqltools/Payroll.cs
+++ src/msamis/ryldb.sqltools/Payroll.cs
@@ -73,7 +73,32 @@ namespace MSAMISUserInterface {
             return HourIterationTotal;
         }
 
+        public static DataTable GetGuardsPayrollList() {
+            return SQLTools.ExecuteQuery(@"select concat(ln,', ',fn,' ',mn) as name, client.name, (
+                                                    CASE 
+                                                        WHEN period.pid IS NULL
+                                                        THEN 'Yes'
+                                                        ELSE 'No Attendance Details Found'
+                                                    END
+                                                  ) AS attendance
+                                             from request
+                                            left join client on client.cid=request.cid
+                                            left join request_assign on request_assign.rid=request.rid
+                                            left join sduty_assignment on sduty_assignment.raid=request_assign.raid
+                                            left join guards on guards.gid=sduty_assignment.gid
+                                            left join period on guards.gid=period.gid
 
+
+                                            where RequestType = 1 and
+                                            ((period=1
+                                            and month = 7
+                                            and year = 2017) OR
+                                            (pid is null))
+                                            group by guards.gid
+                                            ");
+        }
+
+        
         
 
         
