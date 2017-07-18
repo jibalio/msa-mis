f22caa7dac8c6689dc49a021c4dc50e9f83670bb
lerycibalio <lerycibalio@gmail.com>
2017-07-18 09:41:24 +0800

    [Added] GetAssignmentDetails() for contractdetails

--- src/msamis/ryldb.sqltools/Scheduling.cs
+++ src/msamis/ryldb.sqltools/Scheduling.cs
@@ -387,7 +387,13 @@ from guards left join sduty_assignment on guards.gid = sduty_assignment.gid
         #endregion
 
 
-
+        public static DataTable GetAssignmentDetails (int aid) {
+            String q = @"SELECT concat(streetno, ', ', streetname, ', ', brgy, ', ', city) as Location, ContractStart, ContractEnd FROM msadb.sduty_assignment
+                        left join request_assign on request_assign.raid=sduty_assignment.raid
+                        left join request on request.rid=request_assign.rid
+                        where sduty_assignment.aid="+aid+";";
+                                    return SQLTools.ExecuteQuery(q);
+        }
 
         #region View Request Methods
 
