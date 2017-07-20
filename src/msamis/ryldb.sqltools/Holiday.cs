7d968bb8df32d0aa1d78dc3aa2a40b476e680aa8
lerycibalio <lerycibalio@gmail.com>
2017-07-20 10:55:48 +0800

    [Fixed] Holiday type now string

--- src/msamis/ryldb.sqltools/Holiday.cs
+++ src/msamis/ryldb.sqltools/Holiday.cs
@@ -47,7 +47,12 @@ namespace ryldb.sqltools {
         }
 
         public static DataTable GetHolidays() {
-            string q = "select * from holiday";
+            string q = @"SELECT hid, datestart, dateend, `desc`, 
+                        case type
+                        when 1 then 'Regular'
+                        when 2 then 'Special'
+                        end as type
+                        FROM msadb.holiday; ";
             return SQLTools.ExecuteQuery(q);
         }
 
