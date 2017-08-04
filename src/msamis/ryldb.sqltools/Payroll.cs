2b66abf43f231724e7a164fae89e7b9662379a17
lerycibalio <lerycibalio@gmail.com>
2017-08-04 22:04:19 +0800

    [Fixed] Wrong query on GetWithTaxHeaders

--- src/msamis/ryldb.sqltools/Payroll.cs
+++ src/msamis/ryldb.sqltools/Payroll.cs
@@ -609,7 +609,9 @@ WHERE type ={Enumeration.ContribType.Sss} AND status={Enumeration.ContribStatus.
         #region WithTax: DB Ops
 
         public static DataTable GetWithTaxHeaders(int contrib_id) {
-            return SQLTools.ExecuteQuery($@"SELECT * FROM msadb.withtax_value where wid='{contrib_id}';");
+            return SQLTools.ExecuteQuery($@"SELECT wid, value, excessmult, contribdetails.contrib_id FROM msadb.withtax_value 
+right join withtax_bracket on withtax_bracket.taxid=withtax_value.wid
+left join contribdetails on contribdetails.contrib_id=withtax_bracket.contrib_id where contribdetails.contrib_id='{contrib_id}';");
         }
 
         public static DataTable GetWithTaxBrackets(int contrib_id) {
