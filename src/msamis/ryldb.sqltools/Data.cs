b40daaa068ab1a875d6d403e20301602ec2680e4
lerycibalio <lerycibalio@gmail.com>
2017-08-06 23:02:44 +0800

    moved ini to \Configurations

--- src/msamis/ryldb.sqltools/Data.cs
+++ src/msamis/ryldb.sqltools/Data.cs
@@ -1,23 +1,44 @@
 ﻿using System;
 using System.IO;
+using IniParser;
+using IniParser.Model;
 
 namespace MSAMISUserInterface {
     public class Data {
 
-        
-        
+        public static FileIniDataParser iniparser = new FileIniDataParser();
+        public static IniData PayrollIni;
+        private static readonly string PayrollIniLocation = $@"Configuration\\payroll.ini";
+
+        private static readonly string PayrollIniContent =
+            #region + string definition
+            $@"[Payroll]
+DefaultCashAdvance = 0.00
+DefaultPHIC = 100.00
+DefaultHDMF = 100.00
+DefaultCashBond = 150.00
+DefaultCola = 100.00
+DefaultEmer = 50.00";
+        #endregion
         public static void InitData() {
-            
+            _InitInis();
             initRates();
             InitReportsFolder();
         }
 
-        private static void initHourCosts() {
-           
-        }
-
-        public static void UpdateExipiry() {
-
+        private static void _InitInis() {
+            FileInfo fi = new FileInfo(PayrollIniLocation);
+            if (!fi.Directory.Exists) {
+                System.IO.Directory.CreateDirectory(fi.DirectoryName);
+            }
+            string initialini = Data.IniConntent;
+            //Directory.CreateDirectory(Path.GetDirectoryName("Configuration"));
+            if (!File.Exists(PayrollIniLocation)) {
+                using (var writer = new StreamWriter(PayrollIniLocation)) {
+                    writer.WriteLine(PayrollIniContent);
+                }
+            }
+            PayrollIni = iniparser.ReadFile(PayrollIniLocation);
         }
 
         public static void initRates () {
@@ -41,14 +62,6 @@ AlwaysUpdateIni = true
 
 [Reports]
 DefaultDirectory = C:\Docs
-
-[Payroll]
-DefaultCashAdvance = 0.00
-DefaultPHIC = 100.00
-DefaultHDMF = 100.00
-DefaultCashBond = 150.00
-DefaultCola = 100.00
-DefaultEmer = 50.00
 ";
     }
 }
