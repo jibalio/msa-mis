12d760caf0699f921ce6acbab1c429490f469363
lerycibalio <lerycibalio@gmail.com>
2017-08-18 23:12:10 +0800

    unassignment stored proc  + view assignments fix

--- src/msamis/ryldb.sqltools/Data.cs
+++ src/msamis/ryldb.sqltools/Data.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using IniParser;
 using IniParser.Model;
@@ -15,6 +16,8 @@ namespace MSAMISUserInterface {
             InitRates();
             InitReportsFolder();
             InitGuardStatusAndDutyAssignments();
+
+
         }
 
         public static void InitGuardStatusAndDutyAssignments() {
