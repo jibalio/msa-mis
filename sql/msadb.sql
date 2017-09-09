3a7e4725205c80cbd56421021626727af173f5de
Student <lerycibalio@gmail.com>
2017-09-10 03:26:18 +0800

    [Update] Super Final DB

--- sql/msadb.sql
+++ sql/msadb.sql
@@ -1418,6 +1418,24 @@ LOCK TABLES `period` WRITE;
 INSERT INTO `period` VALUES (130,156,09,1,2017,47,14),(134,134,09,1,2017,19,8),(140,110,09,1,2017,8,8),(144,59,09,1,2017,11,11),(148,168,09,1,2017,8,8),(152,104,09,1,2017,45,12),(156,61,09,1,2017,47,14),(160,111,09,1,2017,NULL,14);
 /*!40000 ALTER TABLE `period` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `msadb`.`period_BEFORE_DELETE` BEFORE DELETE ON `period` FOR EACH ROW
+BEGIN
+	delete from attendance where pid = OLD.pid;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `personsinvolved`
@@ -2512,7 +2530,7 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-30  2:08:18
+-- Dump completed on 2017-09-10  3:04:11
 CREATE DATABASE  IF NOT EXISTS `msadbarchive` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `msadbarchive`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
@@ -3293,4 +3311,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-30  2:08:18
+-- Dump completed on 2017-09-10  3:04:12
