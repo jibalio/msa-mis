43bd970b735befde80b8fc3110a56821716d34ec
AJ Pasigado <ajpasigado@outlook.com>
2017-09-11 22:54:53 +0800

    [Added] Remove Feature for Future Rates

--- src/msamis/MSAMISUserInterface/PayrollConfigRates.cs
+++ src/msamis/MSAMISUserInterface/PayrollConfigRates.cs
@@ -184,6 +184,18 @@ namespace MSAMISUserInterface {
             }
         }
 
+
+        private void BasicPayGRD_CellEnter(object sender, DataGridViewCellEventArgs e) {
+            if (BasicPayGRD.SelectedRows.Count > 0 && BasicPayGRD.SelectedRows[0].Cells[4].Value.ToString().Equals("Pending")) {
+                BasicPayCancelBTN.Visible = true;
+                AdjustBTN.Location = new Point(170, 60);
+            }
+            else {
+                BasicPayCancelBTN.Visible = false;
+                AdjustBTN.Location = new Point(201, 60);
+            }
+        }
+
         private void AdjustBTN_Click(object sender, EventArgs e) {
             AdjustPNL.Visible = true;
             CurrentPNL.Visible = false;
@@ -436,6 +448,21 @@ namespace MSAMISUserInterface {
 
         private void SSSDateCMBX_SelectedIndexChanged(object sender, EventArgs e) {
             SssLoadTable();
+            SssFormatLayout();
+        }
+
+        private void SssFormatLayout() {
+            if (SSSDateCMBX.Text.Contains("Pending")) {
+                SSSCancelBTN.Visible = true;
+                SSSDateCMBX.Size = new Size(208, 25);
+                SSSEditBTN.Location = new Point(337, -1);
+            }
+            else {
+                SSSCancelBTN.Visible = false;
+                SSSDateCMBX.Size = new Size(293, 25);
+                SSSEditBTN.Location = new Point(421, -1);
+            }
+
         }
 
         private void SSSEditBTN_Click(object sender, EventArgs e) {
@@ -767,6 +794,22 @@ namespace MSAMISUserInterface {
 
         private void MultipliersDateCMBX_SelectedIndexChanged(object sender, EventArgs e) {
             MultLoadValues();
+            MultFormatPage();
+        }
+
+        private void MultFormatPage() {
+            if (MultipliersDateCMBX.Text.Contains("Pending")) {
+                MultCancelPendingBTN.Visible = true;
+                MultipliersDateCMBX.Size = new Size(249, 25);
+                MultEditBTN.Location = new Point(337, 8);
+            }
+            else {
+                MultCancelPendingBTN.Visible = false;
+                MultipliersDateCMBX.Size = new Size(327, 25);
+                MultEditBTN.Location = new Point(420, 8);
+            }
+
+
         }
 
         private void MultLoadValues() {
@@ -840,8 +883,8 @@ namespace MSAMISUserInterface {
             LoadGlobalPage();
         }
 
-        #endregion
 
+        #endregion
 
     }
 }
